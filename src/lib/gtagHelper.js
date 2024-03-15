84f876e9eb4e3fb8c2a2866af6bcdbd03571aa92
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2024-03-15 14:02:08 -0300

    enviando projeto

--- src/lib/gtagHelper.js
+++ src/lib/gtagHelper.js
@@ -1,7 +1,5 @@
 export const pageview = (GA_MEASUREMENT_ID, url) => {
-  if (typeof window !== 'undefined') {
-    window.gtag("config", GA_MEASUREMENT_ID, {
-      page_path: url,
-    });
-  }
+  window.gtag("config", GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
 };
