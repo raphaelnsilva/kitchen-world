794c923d785796267680eb8ca28cc32f5ddc99cd
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2024-03-15 13:38:19 -0300

    Update gtagHelper.js

--- src/lib/gtagHelper.js
+++ src/lib/gtagHelper.js
@@ -1,9 +1,13 @@
-import "client-only";
+'use client'
+
+import { useEffect } from "react";
 
 export const pageview = (GA_MEASUREMENT_ID, url) => {
-  if (typeof window !== 'undefined') {
+  useEffect(() => {
+    
     window.gtag("config", GA_MEASUREMENT_ID, {
       page_path: url,
     });
-  }
+  
+  }, [GA_MEASUREMENT_ID, url])
 };
