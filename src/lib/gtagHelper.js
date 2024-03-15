6b6f21dd00a0ae4afae8d36d81ff653b33f8a1d6
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2024-03-15 13:47:35 -0300

    Enviando projeto

--- src/lib/gtagHelper.js
+++ src/lib/gtagHelper.js
@@ -1,13 +1,5 @@
-'use client'
-
-import { useEffect } from "react";
-
 export const pageview = (GA_MEASUREMENT_ID, url) => {
-  useEffect(() => {
-    
-    window.gtag("config", GA_MEASUREMENT_ID, {
+  window.gtag("config", GA_MEASUREMENT_ID, {
       page_path: url,
-    });
-  
-  }, [GA_MEASUREMENT_ID, url])
+  });
 };
